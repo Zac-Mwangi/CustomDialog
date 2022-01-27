9e9bcb217c9db241fe4c94d8c8e75e54aa735bfa
Zack M <zackmwangi998@gmail.com>
2022-01-27 11:37:16 +0300

    changes

--- app/src/main/java/com/example/customdialog/ExampleDialog.java
+++ app/src/main/java/com/example/customdialog/ExampleDialog.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,7 @@ public class ExampleDialog extends AppCompatDialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
 
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
